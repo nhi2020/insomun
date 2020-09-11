eea340f0cc9b659340047cb1a837f7da21c69dee
123123 <123123@JW>
2020-09-11 15:51:18 +0900

    123

--- t/src/main/java/egovframework/let/shop/notice/service/Notice.java
+++
@@ -1,8 +0,0 @@
-package egovframework.let.shop.notice.service;
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public class Notice implements Serializable{
-	
-}
--- t/src/main/java/egovframework/let/shop/seller/service/EgovSellerService.java
+++
@@ -1,8 +0,0 @@
-package egovframework.let.shop.seller.service;
-
-import java.util.List;
-
-public interface EgovSellerService {
-	
-	public List<SellerVO> selectSellerList(SellerVO sellerVo) throws Exception;
-}
