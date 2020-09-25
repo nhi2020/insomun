ec50276f854d476cf3b0fa3a7acfc88c84d77b98
user <user@DESKTOP-P7PDH9S>
2020-09-25 17:44:11 +0900

    deal 작업

--- t/src/main/java/egovframework/let/shop/mng/deal/web/DealMngController.java
+++ t/src/main/java/egovframework/let/shop/mng/deal/web/DealMngController.java
@@ -1,5 +1,49 @@
 package egovframework.let.shop.mng.deal.web;
 
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import egovframework.let.shop.mng.basket.service.BasketMngService;
+import egovframework.let.shop.mng.basket.web.BasketMngController;
+import egovframework.rte.fdl.property.EgovPropertyService;
+
+@Controller
 public class DealMngController {
 
+	private static final Logger logger = LoggerFactory.getLogger(BasketMngController.class);
+
+	@Resource(name = "BasketMngService")
+	private BasketMngService basketService;
+
+	@Resource(name = "propertiesService")
+	protected EgovPropertyService propertyService;
+
+	protected String unscript(String data) {
+		if (data == null || data.trim().equals("")) {
+			return "";
+		}
+
+		String ret = data;
+
+		ret = ret.replaceAll("<(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;script");
+		ret = ret.replaceAll("</(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;/script");
+
+		ret = ret.replaceAll("<(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;object");
+		ret = ret.replaceAll("</(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;/object");
+
+		ret = ret.replaceAll("<(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;applet");
+		ret = ret.replaceAll("</(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;/applet");
+
+		ret = ret.replaceAll("<(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
+		ret = ret.replaceAll("</(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
+
+		ret = ret.replaceAll("<(F|f)(O|o)(R|r)(M|m)", "&lt;form");
+		ret = ret.replaceAll("</(F|f)(O|o)(R|r)(M|m)", "&lt;form");
+
+		return ret;
+	}
+	
 }
