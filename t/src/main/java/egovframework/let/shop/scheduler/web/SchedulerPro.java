39b238ecec97d93fd50f1b63d45625620f446dab
jw <herry215@hanmail.net>
2020-10-12 20:55:41 +0900

    log

---
+++ t/src/main/java/egovframework/let/shop/scheduler/web/SchedulerPro.java
@@ -0,0 +1,12 @@
+package egovframework.let.shop.scheduler.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SchedulerPro {
+	
+	public void insertLog() {
+		System.out.println("로그를 저장 합니다.");
+	}
+
+}
