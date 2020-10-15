e4d75f75700a3dd5c32cc7768d4dd4109bd00e85
pjc1991 <68521144+pjc1991@users.noreply.github.com>
2020-10-15 13:18:52 +0900

    listBasket / MngloginInterceptor 주석

--- t/src/main/java/egovframework/let/shop/intercepter/MngLoginInterceptor.java
+++ t/src/main/java/egovframework/let/shop/intercepter/MngLoginInterceptor.java
@@ -1,8 +1,6 @@
 package egovframework.let.shop.intercepter;
 
-import egovframework.com.cmm.LoginVO;
 
-import egovframework.rte.fdl.security.userdetails.util.EgovUserDetailsHelper;
 
 import java.io.IOException;
 
@@ -10,58 +8,47 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.ModelAndViewDefiningException;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
-
-/**
- * 인증여부 체크 인터셉터
- * 
- * @author 공통서비스 개발팀 서준식
- * @since 2011.07.01
- * @version 1.0
- * @see
- *
- *      <pre>
- * << 개정이력(Modification Information) >>
- *
- *   수정일      수정자          수정내용
- *  -------    --------    ---------------------------
- *  2011.07.01  서준식          최초 생성
- *  2011.09.07  서준식          인증이 필요없는 URL을 패스하는 로직 추가
- *  2014.06.11  이기하          인증이 필요없는 URL을 패스하는 로직 삭제(xml로 대체)
- *      </pre>
- */
 
 public class MngLoginInterceptor extends WebContentInterceptor {
 
-	/**
-	 * 세션에 계정정보(LoginVO)가 있는지 여부로 인증 여부를 체크한다. 계정정보(LoginVO)가 없다면, 로그인 페이지로
-	 * 이동한다.
+	/*
+	 * 운영자 로그인 인터셉터 
+	 * 
+	 * 운영자의 로그인 여부를 확인하기 위한 인터셉터
+	 * 인터셉터 작성에 참고하세요
+	 * WebContentInterceptor를 상속 받고 
+	 * egov-com-servlet.xml 파일에서 인터셉터 설정이 필요하다.
+	 * 
+	 * 작성자 : 박종찬
+	 * 작성일 : 2020. 10. 14. 
 	 */
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws ServletException {
 		System.out.println("MngLoginInterceptor");
-		
-		// 세션에서 로그인 정보를 취득
+
+		// 세션을 통헤 로그인 정보를 취득
 		HttpSession session = request.getSession();
 		String a_id = (String) session.getAttribute("A_ID");
-		
+
 		if (a_id == null || a_id.equals("")) {
+			// a_id 가 null 인 경우에는 운영자 로그인이 되어있지 않은 것으로 간주한다.
 			System.out.println("관리자 권한 없음 => 로그인 페이지로 리다이렉트 합니다.");
 			try {
-				response.sendRedirect("/shop/mng/admin/AdminLogin.do");
+				// 로그인 페이지로 리다이렉트 시킨다. (로그인 페이지는 MngLoginInterceptor의 예외 대상으로 지정되어있다.)
 				
+				response.sendRedirect("/shop/mng/admin/AdminLogin.do");
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			// 컨트롤러로 요청을 넘기지 않기 위해 false로 처리한다.
+			// 컨트롤러로 요청을 넘기지 않기 위해 false를 반환한다.
 			return false;
 		}
-		
-		// true로 처리될 경우 인터셉터가 작업된 이후 정상적으로 컨트롤러에 매핑된다.
+
+		// preHandle에서 true를 반환한 경우에는 preHandler 처리 이후 정상적으로 컨트롤러가 호출된다. 
 		return true;
 	}
 
